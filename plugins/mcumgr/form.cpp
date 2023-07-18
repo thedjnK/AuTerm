dd78adc516a63467f12f64814c3571e3c21a87b1
Jamie McCrae <spam@helper3000.net>
2023-07-18 12:00:19 +0100

    plugins: mcumgr: Add additional UI elements

--- plugins/mcumgr/form.cpp
+++ plugins/mcumgr/form.cpp
@@ -18,45 +18,72 @@ void Form::on_btn_FS_Local_clicked()
 
 }
 
-
 void Form::on_btn_FS_Go_clicked()
 {
 
 }
-
 
 void Form::on_radio_FS_Upload_clicked()
 {
 
 }
 
-
 void Form::on_radio_FS_Download_clicked()
 {
 
 }
-
 
 void Form::on_radio_FS_Size_clicked()
 {
 
 }
 
-
 void Form::on_radio_FS_HashChecksum_clicked()
 {
 
 }
-
 
 void Form::on_btn_IMG_Local_clicked()
 {
 
 }
 
-
 void Form::on_btn_IMG_Go_clicked()
 {
 
 }
 
+void Form::on_radio_IMG_No_Action_toggled(bool checked)
+{
+
+}
+
+void Form::on_btn_IMG_Preview_Copy_clicked()
+{
+
+}
+
+void Form::on_btn_OS_Go_clicked()
+{
+
+}
+
+void Form::on_btn_SHELL_Go_clicked()
+{
+
+}
+
+void Form::on_btn_STAT_Go_clicked()
+{
+
+}
+
+void Form::on_btn_SHELL_Clear_clicked()
+{
+
+}
+
+void Form::on_btn_SHELL_Copy_clicked()
+{
+
+}
--- plugins/mcumgr/form.h
+++ plugins/mcumgr/form.h
@@ -22,12 +22,17 @@ private slots:
     void on_radio_FS_Download_clicked();
     void on_radio_FS_Size_clicked();
     void on_radio_FS_HashChecksum_clicked();
-
     void on_btn_IMG_Local_clicked();
-
     void on_btn_IMG_Go_clicked();
+    void on_radio_IMG_No_Action_toggled(bool checked);
+    void on_btn_IMG_Preview_Copy_clicked();
+    void on_btn_OS_Go_clicked();
+    void on_btn_SHELL_Go_clicked();
+    void on_btn_STAT_Go_clicked();
+    void on_btn_SHELL_Clear_clicked();
+    void on_btn_SHELL_Copy_clicked();
 
-    private:
+private:
     Ui::Form *ui;
 };
 
