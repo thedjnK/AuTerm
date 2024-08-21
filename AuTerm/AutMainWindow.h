0318b71428866ee0aac43f54687973ebb565b493
Jamie McCrae <spam@helper3000.net>
2024-08-21 21:31:11 +0100

    Version 0.30 release

--- AuTerm/AutMainWindow.h
+++ AuTerm/AutMainWindow.h
@@ -104,7 +104,7 @@
 // Constants
 /******************************************************************************/
 //Constants for version and functions
-const QString UwVersion                         = "0.29a"; //Version string
+const QString UwVersion                         = "0.30"; //Version string
 //Constants for timeouts and streaming
 const qint16 FileReadBlock                      = 512;     //Number of bytes to read per block when streaming files
 const qint16 StreamProgress                     = 10000;   //Number of bytes between streaming progress updates
